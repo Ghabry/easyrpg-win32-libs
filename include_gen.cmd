978c1ea6917b07b76a758f3d3807597626f2a958
Ghabry <gabelk@web.de>
2013-12-13 23:35:32 +0100

    Update to SDL2.0.1, Step 1

--- include_gen.cmd
+++ include_gen.cmd
@@ -9,13 +9,13 @@ xcopy /Y libmikmod-3.2.0\include\*.h build\include
 xcopy /Y /I libogg-1.3.0\include\ogg\*.h build\include\ogg
 xcopy /Y libpng-1.5.14\*.h build\include
 xcopy /Y /I libvorbis-1.3.3\include\vorbis\*.h build\include\vorbis
-xcopy /Y pixman-0.30.0\pixman\pixman*.h build\include
+xcopy /Y pixman-0.32.4\pixman\pixman*.h build\include
 xcopy /Y zlib-1.2.7\z*.h build\include
 xcopy /Y SDL-1.2.15\include\*.h build\include\SDL1_2\
 xcopy /Y SDL_image-1.2.12\SDL_*.h build\include\SDL1_2
 xcopy /Y SDL_mixer-1.2.12\SDL_*.h build\include\SDL1_2
 xcopy /Y SDL_ttf-2.0.11\SDL_*.h build\include\SDL1_2
-xcopy /Y SDL2-2.0.0\include\*.h build\include\SDL2\
+xcopy /Y SDL2-2.0.1\include\*.h build\include\SDL2\
 xcopy /Y SDL2_image-2.0.0\SDL_*.h build\include\SDL2
 xcopy /Y SDL2_mixer-2.0.0\SDL_*.h build\include\SDL2
 xcopy /Y SDL2_ttf-2.0.12\SDL_*.h build\include\SDL2
