7153efcd1382ab38ac12617530ef5e43aeb0890c
Ghabry <gabelk@web.de>
2015-10-10 15:10:09 +0200

    Rename all folders to get rid of version information (see README instead)

--- build.cmd
+++ build.cmd
@@ -16,53 +16,44 @@ set TARGETTOOLSET=%1
 echo Compiling using toolset %TARGETTOOLSET%
 echo.
 @echo on
-msbuild expat-2.1.0\expat100.sln /t:Clean;expatw_static /p:configuration=Debug /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild freetype-2.4.11\builds\win32\vc2010\freetype.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild jpeg-9\jpeg100\jpeg.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libmad-0.15.1b\msvc++100\libmad.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libmikmod-3.2.0\win32\mikmod100\mikmod.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libogg-1.3.0\win32\VS2010\libogg_static.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libpng-1.5.14\projects\vstudio\vstudio.sln /p:configuration=Debug /t:Clean;libpng /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libvorbis-1.3.3\win32\VS2010\vorbis_static.sln /p:configuration=Debug /t:Clean;libvorbis /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libvorbis-1.3.3\win32\VS2010\vorbis_static.sln /p:configuration=Debug /t:Clean;libvorbisfile /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild pixman-0.32.4\pixman\pixman100\pixman.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL-1.2.15\VisualC100\SDL.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL_image-1.2.12\VisualC100\SDL_image.sln /p:configuration=Debug /t:Clean;SDL_image /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL_mixer-1.2.12\VisualC100\SDL_mixer.sln /p:configuration=Debug /t:Clean;native_midi /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL_mixer-1.2.12\VisualC100\SDL_mixer.sln /p:configuration=Debug /t:Clean;SDL_mixer /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL_ttf-2.0.11\VisualC100\SDL_ttf.sln /p:configuration=Debug /t:Clean;SDL_ttf /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2-2.0.3\VisualC\SDL_VS2010.sln /p:configuration=Debug /t:Clean;SDL2 /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2-2.0.3\VisualC\SDL_VS2010.sln /p:configuration=Debug /t:SDL2main /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2_image-2.0.0\VisualC\SDL_image_VS2010.sln /p:configuration=Debug /t:Clean;SDL2_image /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2_mixer-2.0.0\VisualC\SDL_mixer_VS2010.sln /p:configuration=Debug /t:Clean;native_midi /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2_mixer-2.0.0\VisualC\SDL_mixer_VS2010.sln /p:configuration=Debug /t:Clean;SDL2_mixer /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2_ttf-2.0.12\VisualC\SDL_ttf_VS2010.sln /p:configuration=Debug /t:Clean;SDL2_ttf /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild zlib-1.2.7\win32\zlib100\zlib.sln /t:Clean;Build /p:configuration=Debug /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild expat\expat100.sln /t:Clean;expatw_static /p:configuration=Debug /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild freetype\builds\win32\vc2010\freetype.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild jpeg\jpeg100\jpeg.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libmad\msvc++100\libmad.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libmikmod\win32\mikmod100\mikmod.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libogg\win32\VS2010\libogg_static.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libpng\projects\vstudio\vstudio.sln /p:configuration=Debug /t:Clean;libpng /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libvorbis\win32\VS2010\vorbis_static.sln /p:configuration=Debug /t:Clean;libvorbis /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libvorbis\win32\VS2010\vorbis_static.sln /p:configuration=Debug /t:Clean;libvorbisfile /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild pixman\pixman\pixman100\pixman.sln /p:configuration=Debug /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL_ttf\VisualC100\SDL_ttf.sln /p:configuration=Debug /t:Clean;SDL_ttf /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2\VisualC\SDL_VS2010.sln /p:configuration=Debug /t:Clean;SDL2 /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2\VisualC\SDL_VS2010.sln /p:configuration=Debug /t:SDL2main /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2_image\VisualC\SDL_image_VS2010.sln /p:configuration=Debug /t:Clean;SDL2_image /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2_mixer\VisualC\SDL_mixer_VS2010.sln /p:configuration=Debug /t:Clean;native_midi /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2_mixer\VisualC\SDL_mixer_VS2010.sln /p:configuration=Debug /t:Clean;SDL2_mixer /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2_ttf\VisualC\SDL_ttf_VS2010.sln /p:configuration=Debug /t:Clean;SDL2_ttf /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild zlib\win32\zlib100\zlib.sln /t:Clean;Build /p:configuration=Debug /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
 
-msbuild expat-2.1.0\expat100.sln /t:Clean;expatw_static /p:configuration=Release /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild freetype-2.4.11\builds\win32\vc2010\freetype.sln /t:Clean;Build /p:configuration=Release /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild jpeg-9\jpeg100\jpeg.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libmad-0.15.1b\msvc++100\libmad.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libmikmod-3.2.0\win32\mikmod100\mikmod.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libogg-1.3.0\win32\VS2010\libogg_static.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libpng-1.5.14\projects\vstudio\vstudio.sln /p:configuration=Release /t:Clean;libpng /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libvorbis-1.3.3\win32\VS2010\vorbis_static.sln /p:configuration=Release /t:Clean;libvorbis /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild libvorbis-1.3.3\win32\VS2010\vorbis_static.sln /p:configuration=Release /t:Clean;libvorbisfile /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild pixman-0.32.4\pixman\pixman100\pixman.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL-1.2.15\VisualC100\SDL.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL_image-1.2.12\VisualC100\SDL_image.sln /p:configuration=Release /t:Clean;SDL_image /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL_mixer-1.2.12\VisualC100\SDL_mixer.sln /p:configuration=Release /t:Clean;native_midi /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL_mixer-1.2.12\VisualC100\SDL_mixer.sln /p:configuration=Release /t:Clean;SDL_mixer /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL_ttf-2.0.11\VisualC100\SDL_ttf.sln /p:configuration=Release /t:Clean;SDL_ttf /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2-2.0.3\VisualC\SDL_VS2010.sln /p:configuration=Release /t:Clean;SDL2 /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2-2.0.3\VisualC\SDL_VS2010.sln /p:configuration=Release /t:SDL2main /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2_image-2.0.0\VisualC\SDL_image_VS2010.sln /p:configuration=Release /t:Clean;SDL2_image /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2_mixer-2.0.0\VisualC\SDL_mixer_VS2010.sln /p:configuration=Release /t:Clean;native_midi /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2_mixer-2.0.0\VisualC\SDL_mixer_VS2010.sln /p:configuration=Release /t:Clean;SDL2_mixer /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild SDL2_ttf-2.0.12\VisualC\SDL_ttf_VS2010.sln /p:configuration=Release /t:Clean;SDL2_ttf /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
-msbuild zlib-1.2.7\win32\zlib100\zlib.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild expat\expat100.sln /t:Clean;expatw_static /p:configuration=Release /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild freetype\builds\win32\vc2010\freetype.sln /t:Clean;Build /p:configuration=Release /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild jpeg\jpeg100\jpeg.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libmad\msvc++100\libmad.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libmikmod\win32\mikmod100\mikmod.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libogg\win32\VS2010\libogg_static.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libpng\projects\vstudio\vstudio.sln /p:configuration=Release /t:Clean;libpng /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libvorbis\win32\VS2010\vorbis_static.sln /p:configuration=Release /t:Clean;libvorbis /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild libvorbis\win32\VS2010\vorbis_static.sln /p:configuration=Release /t:Clean;libvorbisfile /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild pixman\pixman\pixman100\pixman.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2\VisualC\SDL_VS2010.sln /p:configuration=Release /t:Clean;SDL2 /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2\VisualC\SDL_VS2010.sln /p:configuration=Release /t:SDL2main /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2_image\VisualC\SDL_image_VS2010.sln /p:configuration=Release /t:Clean;SDL2_image /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2_mixer\VisualC\SDL_mixer_VS2010.sln /p:configuration=Release /t:Clean;native_midi /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2_mixer\VisualC\SDL_mixer_VS2010.sln /p:configuration=Release /t:Clean;SDL2_mixer /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild SDL2_ttf\VisualC\SDL_ttf_VS2010.sln /p:configuration=Release /t:Clean;SDL2_ttf /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
+msbuild zlib\win32\zlib100\zlib.sln /p:configuration=Release /t:Clean;Build /p:PlatformToolset=%TARGETTOOLSET% || goto :EOF
 
-pushd icu4c-53.1
+pushd icu4c
 call build_icu.cmd
 popd
 
--- include_gen.cmd
+++ include_gen.cmd
@@ -1,23 +1,19 @@
 @echo off
 echo EasyRPG Include File Copier
 
-xcopy /Y /I expat-2.1.0\lib\expat*.h build\include
-xcopy /Y /S freetype-2.4.11\include build\include
-xcopy /Y jpeg-9\j*.h build\include
-xcopy /Y libmad-0.15.1b\*.h build\include
-xcopy /Y libmikmod-3.2.0\include\*.h build\include
-xcopy /Y /I libogg-1.3.0\include\ogg\*.h build\include\ogg
-xcopy /Y libpng-1.5.14\*.h build\include
-xcopy /Y /I libvorbis-1.3.3\include\vorbis\*.h build\include\vorbis
-xcopy /Y pixman-0.32.4\pixman\pixman*.h build\include
-xcopy /Y zlib-1.2.7\z*.h build\include
-xcopy /Y SDL-1.2.15\include\*.h build\include\SDL1_2\
-xcopy /Y SDL_image-1.2.12\SDL_*.h build\include\SDL1_2
-xcopy /Y SDL_mixer-1.2.12\SDL_*.h build\include\SDL1_2
-xcopy /Y SDL_ttf-2.0.11\SDL_*.h build\include\SDL1_2
-xcopy /Y SDL2-2.0.3\include\*.h build\include\SDL2\
-xcopy /Y SDL2_image-2.0.0\SDL_*.h build\include\SDL2
-xcopy /Y SDL2_mixer-2.0.0\SDL_*.h build\include\SDL2
-xcopy /Y SDL2_ttf-2.0.12\SDL_*.h build\include\SDL2
-xcopy /Y /I icu4c-53.1\source\common\unicode build\include\unicode
-xcopy /Y /I icu4c-53.1\source\i18n\unicode build\include\unicode
+xcopy /Y /I expat\lib\expat*.h build\include
+xcopy /Y /S freetype\include build\include
+xcopy /Y jpeg\j*.h build\include
+xcopy /Y libmad\*.h build\include
+xcopy /Y libmikmod\include\*.h build\include
+xcopy /Y /I libogg\include\ogg\*.h build\include\ogg
+xcopy /Y libpng\*.h build\include
+xcopy /Y /I libvorbis\include\vorbis\*.h build\include\vorbis
+xcopy /Y pixman\pixman\pixman*.h build\include
+xcopy /Y zlib\z*.h build\include
+xcopy /Y SDL2\include\*.h build\include\SDL2\
+xcopy /Y SDL2_image\SDL_*.h build\include\SDL2
+xcopy /Y SDL2_mixer\SDL_*.h build\include\SDL2
+xcopy /Y SDL2_ttf\SDL_*.h build\include\SDL2
+xcopy /Y /I icu4c\source\common\unicode build\include\unicode
+xcopy /Y /I icu4c\source\i18n\unicode build\include\unicode
